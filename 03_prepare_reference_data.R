0776eae150942146dc9606b3af1a451a60689ed6
streckereck <colin.ferster@gmail.com>
2026-03-26 15:41:40 -0700

    add reference data pre-processing

--- 03_prepare_reference_data.R
+++ 03_prepare_reference_data.R
@@ -2,3 +2,35 @@
 # Prepare the cleaned reference dataset for modelling.
 # Includes deduplication, classification cleanup, CRS transform,
 # block assignment, and clipping to the study area.
+
+reference <- st_read("data_raw/ref_data_spatial.gpkg") %>%
+  st_transform(utm_11) %>%
+  select(
+    "SegmentID",                           
+    "Status",
+    "stratum",                             
+    "block_id" = "block_id_final",
+    "class"= "classification",
+    "reviewer",
+    "bks_desg"= "bikes_desig_raw",
+    "rd_pth" = "Road.or.path",                        
+    "pth_sfc" = "path_surface_raw",
+    "ped_sep" = "ped_sep_raw",
+    "n_lanes" = "lanes_raw",
+    "tfc_clm" = "calming_raw",
+    "bk_lane" = "painted_lane_raw",
+    "bk_buff" = "lane_buffer_raw",
+    "pvd_shldr" = "shoulder_raw",
+    "prkd_crs" = "Parked.cars",
+    "rating" = "Is.this.a.good.place.to.ride.a.bike.",
+    "rvw_cmmt" = "Comment..optional."                  
+  )
+
+reference_study_area <- reference %>%
+  st_intersection(city_sb) %>%
+  filter(
+    ! class %in% c("UNKNOWN",
+                   "No streetview",
+                   "Not enough evidence to evaluate",
+                   NA)
+  )
